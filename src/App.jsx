1164c36fa0731b0416c1b1e61b19fde1a5cd2a54
Nazish Parvez 👨‍💻 <166379592+nazish-parvez@users.noreply.github.com>
2024-11-10 12:00:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-9 py-6 rounded-xl">
 
-            <button onClick={() => setColor("red")} className="outline-none px-6 bg-[red] py-4 rounded-lg text-black font-bold shadow-3xl">Red</button>
+            <button onClick={() => setColor("red")} className="outline-none px-6 bg-[red] py-4 rounded text-black font-bold shadow-3xl">Red</button>
 
             <button onClick={() => setColor("orange")} className="outline-none px-6 bg-[orange] py-4 rounded-lg text-black font-bold shadow-3xl">Orange</button>
 
@@ -23,9 +23,9 @@ function App() {
 
             <button onClick={() => setColor("blue")} className="outline-none px-6 bg-[blue] py-4 rounded-lg text-black font-bold shadow-3xl">Blue</button>
 
-            <button onClick={() => setColor("indigo ")} className="outline-none px-6 bg-[indigo] px-4 rounded-lg text-black font-bold shadow-3xl">Indigo </button>
+            <button onClick={() => setColor("indigo ")} className="outline-none px-6 bg-[indigo] py-4 rounded-lg text-black font-bold shadow-3xl">Indigo </button>
 
-            <button onClick={() => setColor("violet ")} className="outline-none px-6 bg-[violet] py-3 rounded-lg text-black font-bold shadow-3xl">Violet</button>
+            <button onClick={() => setColor("violet ")} className="outline-none px-6 bg-[violet] py-4 rounded-lg text-black font-bold shadow-3xl">Violet</button>
           </div>
 
         </div>
